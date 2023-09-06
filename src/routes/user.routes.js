c24b03b34292c5fc1c2a017e26afb6a225a6b974
Arnab Das <86297998+arnabdas18@users.noreply.github.com>
2023-09-06 17:30:40 +0530

    modified: userRoute and userController with updateUserProfile feature

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { authUser, registerUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", registerUser);
 userRouter.post("/login", authUser);
+userRouter.post("/profile", protect, updateUserProfile);
 
 export default userRouter;
